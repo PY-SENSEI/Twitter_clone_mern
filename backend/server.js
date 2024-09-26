d777c8d0a25ab2412145b815066c13f3671aaa73
PY-SENSEI <gauravjena48@gmail.com>
2024-09-26 10:56:12 +0530

    1_project setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.route.js"
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+console.log(process.env.MONGO_URI)
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+    connectMongoDB();
+})
