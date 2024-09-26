4cbd5042869b273814b5523504cc1a4250ee9301
PY-SENSEI <gauravjena48@gmail.com>
2024-09-26 18:58:44 +0530

    1-auth controller added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,106 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateTokenAndSetCookie.js";
+
 export const signup = async (req, res) => {
-    res.send('signup')
-}
+  try {
+    const { fullName, username, email, password } = req.body;
+
+    const emailRegex =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    const existingUser = await User.findOne({ username})
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(409).json({ message: "Email already exists" });
+    }
+    //hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(500).json({ message: "Failed to create user" });
+    }
+  } catch (error) {
+    console.log("Error in the signup controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const login = async (req, res) => {
-    res.send('signup')
-}
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || '')
+    
+    if(!user || !isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+    return res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        email: user.email,
+        followers: user.followers,
+        following: user.following,
+        profileImg: user.profileImg,
+        coverImg: user.coverImg,
+    });
+
+  } catch (error) {
+    console.log("Error in the login controller", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export const logout = async (req, res) => {
-    res.send('signup')
-}
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+        console.log("Error in the logout controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+  
+};
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        res.status(200).json(user);
+    } catch (error) {
+        console.log("Error in the getMe controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js"
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -7,11 +8,16 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-
+const PORT = process.env.PORT || 8000;
 
 console.log(process.env.MONGO_URI)
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
 
