af7e8ce4587e13fce4cec4323160aaea160699a5
PY-SENSEI <gauravjena48@gmail.com>
2024-09-28 18:59:30 +0530

    1- post routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectMongoDB from "./db/connectMongoDB.js";
 
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 
 
 dotenv.config();
@@ -30,6 +31,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
